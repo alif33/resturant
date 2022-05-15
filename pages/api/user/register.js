da07c2bdbabfaf526b65e95fd1bf4590efb42141
alif33 <alifhasan332@gmail.com>
2022-05-15 10:08:40 +0600

    ...

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -9,22 +9,43 @@ const handler = nc();
 handler.post(async (req, res) => {
   const { name, email, password } = req.body;
   await db.connect();
-  User.find({ email })
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const _user = new User({
-    name, 
-    email,
-    password: hashedPassword
-  });
+  User.find({ email }).exec(async (error, user) => {
+    if (user) {
+      res.status(400).json({
+        success: false,
+        message: "User already registered",
+      });
+    }else{
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const _user = new User({
+        name, 
+        email,
+        password: hashedPassword
+      });
+    
+      _user.save((err, user)=>{
+        if (err) {
+          res.status(400).json({
+            message: "Something went wrong",
+          });
+        }
 
-  const user = await _user.save();
-  await db.disconnect();
+        if (user) {
+          const token = signToken(user);
+          res.status(201).json({
+            success: true,
+            message: 'Registered successfully',
+            token,
+            user
+          });
+        }
 
-  res.send({
-      success: true,
-      message: 'Registered successfully'
-  });
+      });
+    }
+    return await db.disconnect();
+  })
+
 });
 
 export default handler;
