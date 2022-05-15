2a25ce12882b929c0ffd82a6b587a3059d84dee8
alif33 <alifhasan332@gmail.com>
2022-05-15 09:49:31 +0600

    ...

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -9,6 +9,7 @@ const handler = nc();
 handler.post(async (req, res) => {
   const { name, email, password } = req.body;
   await db.connect();
+  User.find({ email })
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   const _user = new User({
