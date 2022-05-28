86813a7a55b50e6e9dc385a890ea62f6bd633e11
Jahid Hasan <jahidhasan2856@gmail.com>
2022-05-28 23:14:05 +0600

    some thing update

--- pages/api/admin/shop/[shopId].js
+++ pages/api/admin/shop/[shopId].js
@@ -2,7 +2,26 @@ import nc from "next-connect";
 import Shop from "../../../../models/Shop";
 import db from "../../../../utils/db";
 
-const handler = nc();
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const config = {
+  api: {
+      bodyParser: false,
+  },
+};
+
+export const handler = nc();
+export const upload = multer();
+
+
 
 handler.get(async (req, res) => {
     try{
@@ -19,21 +38,22 @@ handler.get(async (req, res) => {
 })
 
 
-handler.put(async (req, res) => {
-  try {
-    await db.connect();
-    const shop = await Shop.findByIdAndUpdate(
-      { _id: req.query.shopId },
-      { $set: req.body },
-      { new: true, useFindAndModify: false }
-    );
-    await db.disconnect();
-    res.status(200).json(shop);
-  } catch (err) {
-    res.status(500).json({
-      error: "Server side error",
-    });
-  }
-});
+// handler.put(async (req, res) => {
+//   try {
+//     await db.connect();
+//     const shop = await Shop.findByIdAndUpdate(
+//       { _id: req.query.shopId },
+//       { $set: req.body }
+      
+//     );
+//     await db.disconnect();
+//     res.status(200).json(shop);
+//   } catch (err) {
+//     res.status(500).json({
+//       error: "Server side error",
+//     });
+//   }
+// });
+
 
 export default handler;
