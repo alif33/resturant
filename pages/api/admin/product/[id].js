9d8c5a82a342a7b8539c1c48435fc7c71a6c73e8
Jahid Hasan <jahidhasan2856@gmail.com>
2022-06-20 16:12:03 +0600

    product copied api done

--- pages/api/admin/product/[id].js
+++ pages/api/admin/product/[id].js
@@ -2,7 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 import nc from "next-connect";
 import streamifier from "streamifier";
+import Category from "../../../../models/Category";
 import Product from "../../../../models/Product";
+import Shop from "../../../../models/Shop";
 import { isAdmin } from "../../../../utils/auth";
 import db from "../../../../utils/db";
 
@@ -26,10 +28,77 @@ const upload = multer();
 handler.get(async (req, res) => {
   try {
     await db.connect();
-    const product = await Product.findById({ _id: req.query.id });
+    const product = await Product.findById({ _id: req.query.id }).populate(
+      "category shop",
+      "categoryName shop_name",
+      { Shop, Category }
+    );
     await db.disconnect();
     res.status(200).json(product);
   } catch (err) {
+    res.status(500).json({
+      error: "Server side error",
+    });
+  }
+});
+// copy the product
+
+handler.use(isAdmin).patch(async (req, res) => {
+  try {
+    await db.connect();
+    const product = await Product.findById({ _id: req.query.id });
+
+    const {
+      product_name,
+      description,
+      shop,
+      category,
+      image,
+      options,
+      catalog,
+      property,
+    } = product;
+    if (product) {
+      const newProduct = new Product({
+        product_name,
+        description,
+        category,
+        image: image,
+        options,
+        shop,
+        catalog: {
+          product_type: {
+            cata_title: catalog.product_type.cata_title,
+            cata_price: catalog.product_type.cata_price,
+          },
+        },
+
+        property: {
+          property_name: property.property_name,
+          limit: property.limit,
+          property_option: property.property_option,
+          selection: {
+            sele_name: property.selection.sele_name,
+            large_price: property.selection.large_price,
+            xlarge_price: property.selection.xlarge_price,
+          },
+        },
+      });
+      if (await newProduct.save()) {
+        await db.disconnect();
+        res.send({
+          success: true,
+          message: "Product copied",
+        });
+      }else{
+        res.send({error: "erver side error"})
+      }
+    }else{
+      res.send({error: "Product not found"});
+    }
+   
+  } catch (err) {
+    console.log(err)
     res.status(500).json({
       error: "Server side error",
     });
@@ -55,7 +124,6 @@ handler.use(isAdmin, upload.single("image")).put(async (req, res) => {
     xlarge_price,
   } = req.body;
 
-  console.log(property_option);
 
   //cloudinary upload file streamifier
 
